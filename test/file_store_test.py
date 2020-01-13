35748b4ff699aa99432ef62d053b83404a3abf7d
Karandeep Nagra <karandeepsnagra@gmail.com>
2020-01-12 23:00:44 -0500

    Add test_delete_note

--- test/file_store_test.py
+++ test/file_store_test.py
@@ -57,6 +57,19 @@ class TestFileStore(TestCase):
             ["2013", "5", "My third note."],
         ], result
 
+    def test_delete_note(self):
+        all_notes = "1 2001 note1\n" + \
+            "2 2002 note2\n" + \
+            "3 2003 note3\n"
+        mo = mock_open(read_data=all_notes)
+        with patch('src.data.file_store.open', mo):
+            result = self.file_store.delete_note("2")
+        assert mo.call_count == 2
+        r = mo().readlines
+        w = mo().writelines
+        r.assert_called_once()
+        w.assert_called_once_with(["1 2001 note1\n", "3 2003 note3\n"])
+
 
 if __name__ == '__main__':
     unittest.main()
